1751f76a31580bdcc47dfcbc55164e92c53340cf
Karen Houghton <k.houghton@gns.cri.nz>
2025-07-03 11:02:20 +1200

    help! edited

--- install.r
+++ install.r
@@ -1,2 +1,3 @@
 if (!require("pacman")) install.packages("pacman", repos="http://cran.r-project.org")
 pacman::p_load(rmarkdown, languageserver, ggplot2, palmerpenguins)    # Install & load packages
+install.packages("languageserver")
